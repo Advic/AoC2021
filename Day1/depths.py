0810d567718895e08c8dfecbabcb9a5751aea584
Advic <loktinmartinho@gmail.com>
2021-12-03 13:23:09 -0800

    Day 1 part 2

--- Day1/depths.py
+++ Day1/depths.py
@@ -7,5 +7,16 @@ def depth_scan(depths: np.array) -> int:
     return ((depths[1:] - depths[:-1]) > 0).sum()
 
 
+def sliding_depth_scan(depth_sums: np.array, window_size: int) -> int:
+    depth_sums = np.array([depth_sums[i:len(depth_sums) - (window_size - i) + 1] for i in range(window_size)]).sum(
+        axis=0)
+    return ((depth_sums[1:] - depth_sums[:-1]) > 0).sum()
+
+
 if __name__ == '__main__':
-    print(depth_scan(np.loadtxt('input.txt')))
+    data = np.loadtxt('input.txt')
+    print("part A")
+    print(depth_scan(data))
+    print()
+    print("part B")
+    print(sliding_depth_scan(data, 3))
--- Day1/test_depths.py
+++ Day1/test_depths.py
@@ -2,12 +2,15 @@ import unittest
 
 import numpy as np
 
-from depths import depth_scan
+from depths import depth_scan, sliding_depth_scan
 
 
 class TestSonarSweep(unittest.TestCase):
-    def test_sonar_sweep(self):
+    def test_depth_scan(self):
         self.assertEqual(depth_scan(np.array([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])), 7)
+
+    def test_sliding_depth_scan(self):
+        self.assertEqual(sliding_depth_scan(np.array([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]), 3), 5)
 
 
 if __name__ == '__main__':
