e5ee3e2b5025d8f94e0271bf750f7e1e2d6f320f
Advic <loktinmartinho@gmail.com>
2021-12-03 13:32:54 -0800

    Day 2 Part 2

--- Day2/submarine.py
+++ Day2/submarine.py
@@ -2,21 +2,21 @@
 
 class Submarine:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.position = 0
+        self.depth = 0
 
     def forward(self, d):
-        self.x += d
+        self.position += d
 
     def down(self, d):
-        self.y -= d
+        self.depth += d
 
     def up(self, d):
-        self.y += d
+        self.depth -= d
 
     @property
     def score(self):
-        return abs(self.x * self.y)
+        return abs(self.position * self.depth)
 
     def parse(self, command: str):
         action, num = command.split(' ', 1)
@@ -33,12 +33,34 @@ class Submarine:
                 raise Exception("Unknown command")
 
 
+class AimingSubmarine(Submarine):
+    def __init__(self):
+        super().__init__()
+        self.aim = 0
+
+    def forward(self, d):
+        self.position += d
+        self.depth += self.aim * d
+
+    def down(self, d):
+        self.aim += d
+
+    def up(self, d):
+        self.aim -= d
+
+
 if __name__ == '__main__':
     with open('input.txt') as f:
         lines = f.readlines()
 
+    print("Part A")
     sub = Submarine()
     for line in lines:
         sub.parse(line)
-
     print(sub.score)
+
+    print("Part B")
+    aimingsub = AimingSubmarine()
+    for line in lines:
+        aimingsub.parse(line)
+    print(aimingsub.score)
--- Day2/test_submarine.py
+++ Day2/test_submarine.py
@@ -1,28 +1,55 @@
 import unittest
 
-from submarine import Submarine
+from submarine import Submarine, AimingSubmarine
 
 
-class MyTestCase(unittest.TestCase):
+class SubmarineTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.submarine = Submarine()
 
     def test_parse_forward(self):
         self.submarine.parse("forward 3")
-        self.assertEqual(3, self.submarine.x)  # add assertion here
+        self.assertEqual(3, self.submarine.position)  # add assertion here
 
     def test_parse_up(self):
         self.submarine.parse("up 2")
-        self.assertEqual(2, self.submarine.y)  # add assertion here
+        self.assertEqual(-2, self.submarine.depth)  # add assertion here
 
     def test_parse_down(self):
         self.submarine.parse("down 1")
-        self.assertEqual(-1, self.submarine.y)  # add assertion here
+        self.assertEqual(1, self.submarine.depth)  # add assertion here
 
     def test_score(self):
         self.submarine.parse("forward 5")
         self.submarine.parse("down 3")
         self.assertEqual(15, self.submarine.score)
+
+    def test_input(self):
+        commands = ["forward 5",
+                    "down 5",
+                    "forward 8",
+                    "up 3",
+                    "down 8",
+                    "forward 2"]
+        for command in commands:
+            self.submarine.parse(command)
+        self.assertEqual(150, self.submarine.score)
+
+
+class AimingSubmarineTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.submarine = AimingSubmarine()
+
+    def test_input(self):
+        commands = ["forward 5",
+                    "down 5",
+                    "forward 8",
+                    "up 3",
+                    "down 8",
+                    "forward 2"]
+        for command in commands:
+            self.submarine.parse(command)
+        self.assertEqual(900, self.submarine.score)
 
 
 if __name__ == '__main__':
