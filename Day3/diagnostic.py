b1309e646bb499372d9d99e1283a210acf03e0b6
Advic <loktinmartinho@gmail.com>
2021-12-03 18:14:43 -0800

    Day 3 Part 2

--- Day3/diagnostic.py
+++ Day3/diagnostic.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 
+def bitstring_to_int(bitstring: str):
+    return int(bitstring, 2)
+
+
 def create_boolean_array(report: str) -> np.array:
     return np.array([np.array(list(map(lambda x: int(x), list(line)))) for line in report.split('\n')])
 
@@ -14,7 +18,7 @@ def calculate_gamma_rate(report: str) -> int:
             bitstring += '1'
         else:
             bitstring += '0'
-    return int(bitstring, 2)
+    return bitstring_to_int(bitstring)
 
 
 def calculate_epsilon_rate(report: str) -> int:
@@ -26,15 +30,55 @@ def calculate_epsilon_rate(report: str) -> int:
             bitstring += '0'
         else:
             bitstring += '1'
-    return int(bitstring, 2)
+    return bitstring_to_int(bitstring)
 
 
 def calculate_power_consumption(report: str) -> int:
     return calculate_gamma_rate(report) * calculate_epsilon_rate(report)
 
 
+def calculate_oxygen_generator_rating(report: np.array) -> int:
+    report_trim = np.copy(report)
+    for bitpos in range(report.shape[1]):
+        if report_trim.shape[0] == 1:
+            break
+        frequency_dict = get_frequency_dict(report_trim, bitpos)
+        most_common_value = 1 if frequency_dict.get(1) >= frequency_dict.get(0) else 0
+        report_trim = report_trim[np.where(report_trim[:, bitpos] == most_common_value), :][0]
+    assert report_trim.shape[0] == 1
+    assert report_trim.shape[1] == report.shape[1]
+    return bitstring_to_int(''.join(map(str, report_trim[0, :].tolist())))
+
+
+def calculate_co2_scrubber_rating(report: np.array) -> int:
+    report_trim = np.copy(report)
+    for bitpos in range(report.shape[1]):
+        if report_trim.shape[0] == 1:
+            break
+        frequency_dict = get_frequency_dict(report_trim, bitpos)
+        least_common_value = 1 if frequency_dict.get(1) < frequency_dict.get(0) else 0
+        report_trim = report_trim[np.where(report_trim[:, bitpos] == least_common_value), :][0]
+    assert report_trim.shape[0] == 1
+    assert report_trim.shape[1] == report.shape[1]
+    return bitstring_to_int(''.join(map(str, report_trim[0, :].tolist())))
+
+
+def calculate_life_support_rating(report: np.array) -> int:
+    return calculate_oxygen_generator_rating(report) * calculate_co2_scrubber_rating(report)
+
+
+def get_frequency_dict(report: np.array, bitposition: int) -> dict:
+    assert report.ndim == 2
+    assert np.all(np.logical_xor(report == 0, report == 1))
+    return dict(zip(*np.unique(report[:, bitposition], return_counts=True)))
+
+
 if __name__ == '__main__':
     with open('input.txt') as f:
         report = f.read()
 
+    print("Part 1")
     print(calculate_power_consumption(report))
+    print()
+    print("Part 2")
+    print(calculate_life_support_rating(create_boolean_array(report)))
--- Day3/test_diagnostic.py
+++ Day3/test_diagnostic.py
@@ -5,24 +5,43 @@ import diagnostic
 
 
 class MyTestCase(unittest.TestCase):
+    TEST_INPUT = dedent("""\
+                00100
+                11110
+                10110
+                10111
+                10101
+                01111
+                00111
+                11100
+                10000
+                11001
+                00010
+                01010""")
+
+    TEST_INPUT_ARR = diagnostic.create_boolean_array(TEST_INPUT)
 
     def test_sample(self):
-        diag_data = dedent("""\
-            00100
-            11110
-            10110
-            10111
-            10101
-            01111
-            00111
-            11100
-            10000
-            11001
-            00010
-            01010""")
-        self.assertEqual(diagnostic.calculate_gamma_rate(diag_data), 22)
-        self.assertEqual(diagnostic.calculate_epsilon_rate(diag_data), 9)
-        self.assertEqual(diagnostic.calculate_power_consumption(diag_data), 198)
+        self.assertEqual(diagnostic.calculate_gamma_rate(self.TEST_INPUT), 22)
+        self.assertEqual(diagnostic.calculate_epsilon_rate(self.TEST_INPUT), 9)
+        self.assertEqual(diagnostic.calculate_power_consumption(self.TEST_INPUT), 198)
+
+    def test_get_frequency_dict(self):
+        arr = diagnostic.create_boolean_array(self.TEST_INPUT)
+        self.assertDictEqual(diagnostic.get_frequency_dict(arr, 0), {0: 5, 1: 7})
+        self.assertDictEqual(diagnostic.get_frequency_dict(arr, 1), {0: 7, 1: 5})
+        self.assertDictEqual(diagnostic.get_frequency_dict(arr, 2), {0: 4, 1: 8})
+        self.assertDictEqual(diagnostic.get_frequency_dict(arr, 3), {0: 5, 1: 7})
+        self.assertDictEqual(diagnostic.get_frequency_dict(arr, 4), {0: 7, 1: 5})
+
+    def test_calculate_oxygen_generator_rating(self):
+        self.assertEqual(diagnostic.calculate_oxygen_generator_rating(self.TEST_INPUT_ARR), 23)
+
+    def test_calculate_c02_scrubber_rating(self):
+        self.assertEqual(diagnostic.calculate_co2_scrubber_rating(self.TEST_INPUT_ARR), 10)
+
+    def test_calculate_life_support_rating(self):
+        self.assertEqual(diagnostic.calculate_life_support_rating(self.TEST_INPUT_ARR), 230)
 
 
 if __name__ == '__main__':
