dabd3d4f18b46dbbad37317e0c12f1c0ba5cddb3
Advic <loktinmartinho@gmail.com>
2021-12-09 21:49:57 -0800

    Day10 Part2

--- Day10/syntax.py
+++ Day10/syntax.py
@@ -6,7 +6,7 @@ class SyntaxValidationException(Exception):
 
 
 def validate_line(line: str) -> str:
-    """Return invalid closing character if any is found"""
+    """Return invalid closing character if any is found, else return str of unclosed braces"""
     stack = []
     close_open_map = {
         ')': '(',
@@ -23,7 +23,7 @@ def validate_line(line: str) -> str:
                     return chr
             case _:
                 raise Exception(f"Invalid character %s detected" % chr)
-    return ''
+    return ''.join(stack)
 
 
 def score_syntax(lines: List[str]) -> int:
@@ -31,10 +31,42 @@ def score_syntax(lines: List[str]) -> int:
         ')': 3,
         ']': 57,
         '}': 1197,
-        '>': 25137,
-        '': 0}
-    return sum(score_map[validate_line(line)] for line in lines)
+        '>': 25137}
+    return sum(score_map.get(validate_line(line), 0) for line in lines)
+
+
+def score_incomplete_lines(lines: List[str]) -> int:
+    score_map = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
+
+    open_close_map = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>'
+    }
+
+    scores = []
+    for line in lines:
+        line_total = 0
+        unclosed = validate_line(line)
+        if unclosed in ')]}>':
+            continue
+        for chr in reversed(unclosed):
+            line_total *= 5
+            line_total += score_map[open_close_map[chr]]
+        scores.append(line_total)
+    scores.sort()
+    return scores[int(len(scores) / 2)]
 
 
 if __name__ == '__main__':
+    print("Part 1")
     print(score_syntax([line.strip() for line in open('input.txt').readlines()]))
+    print()
+    print("Part 2")
+    print(score_incomplete_lines([line.strip() for line in open('input.txt').readlines()]))
--- Day10/test_syntax.py
+++ Day10/test_syntax.py
@@ -25,6 +25,9 @@ class MyTestCase(unittest.TestCase):
     def test_score_syntax(self):
         self.assertEqual(26397, syntax.score_syntax(self.TEST_SYNTAX_LINES.split('\n')))
 
+    def test_score_incomplete_lines(self):
+        self.assertEqual(288957, syntax.score_incomplete_lines(self.TEST_SYNTAX_LINES.split('\n')))
+
 
 if __name__ == '__main__':
     unittest.main()
